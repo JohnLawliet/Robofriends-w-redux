08345d226a920d170b3a1e9d5ab1a7fcda8b60f3
JohnLawliet <t.shiladitya@gmail.com>
2020-11-07 15:09:42 +0530

    Updates

---
+++ precache-manifest.2ddf797206119c4212dc9cefab1b2193.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "123a1bc9a9db67985b1a3d8f1b089255",
+    "url": "/Robofriends-w-redux/index.html"
+  },
+  {
+    "revision": "8e853991b805418b0482",
+    "url": "/Robofriends-w-redux/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "6ab15e25f7037a4e305c",
+    "url": "/Robofriends-w-redux/static/css/main.85ca8152.chunk.css"
+  },
+  {
+    "revision": "8e853991b805418b0482",
+    "url": "/Robofriends-w-redux/static/js/2.bc368ef2.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Robofriends-w-redux/static/js/2.bc368ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ab15e25f7037a4e305c",
+    "url": "/Robofriends-w-redux/static/js/main.a0e2e5f3.chunk.js"
+  },
+  {
+    "revision": "5c944dae84aa2be432f5",
+    "url": "/Robofriends-w-redux/static/js/runtime-main.6e435b1e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends-w-redux/static/media/SEGA.12a3db8b.woff"
+  }
+]);
